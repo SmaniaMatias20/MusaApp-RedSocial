fd2f120d4cb3456eb2a4d7a7bdfaf5fef07e293d
Matias <matiasezequielsmania@gmail.com>
2025-06-02 14:00:26 -0300

    modulos back

--- musa-back/src/app.module.ts
+++ musa-back/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
+    UsersModule,
+    PostsModule,
+  ],
 })
-export class AppModule {}
+export class AppModule { }
