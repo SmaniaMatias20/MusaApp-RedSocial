3f0359e50b20a449b627cf0cd476453a29f01572
Matias <matiasezequielsmania@gmail.com>
2025-06-07 11:16:54 -0300

    .

--- musa-back/src/users/users.controller.ts
+++ musa-back/src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -29,10 +28,10 @@ export class UsersController {
         return this.usersService.findOne(id);
     }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.usersService.update(id, updateUserDto);
-    }
+    // @Put(':id')
+    // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    //     return this.usersService.update(id, updateUserDto);
+    // }
 
     @Delete(':id')
     remove(@Param('id') id: string) {
--- musa-back/src/users/users.service.ts
+++ musa-back/src/users/users.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -33,11 +33,11 @@ export class UsersService {
         return user;
     }
 
-    async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-        const updated = await this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
-        if (!updated) throw new NotFoundException('Usuario no encontrado para actualizar');
-        return updated;
-    }
+    // async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    //     const updated = await this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true }).exec();
+    //     if (!updated) throw new NotFoundException('Usuario no encontrado para actualizar');
+    //     return updated;
+    // }
 
     async remove(id: string): Promise<void> {
         const result = await this.userModel.findByIdAndDelete(id).exec();
